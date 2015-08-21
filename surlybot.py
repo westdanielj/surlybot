81d3ca1366c9e00c3579dfca5aee56e8cedd701e
dwest-galois <dwest@galois.com>
2015-08-21 08:58:41 -0700

    Added default values

--- surlybot.py
+++ surlybot.py
@@ -2,15 +2,14 @@
 #
 # surlybot.py
 #
-# This is a simple script that will sing the famount "99 bottles of beer" song
+# This is a simple script that will sing the famous "99 bottles of beer" song.  It will default to starting with
+# 99 bottles of beer, but you can add optional arguments for changing the beverage type and starting number.
 #
 # 08/14/2015 - Dan West - Initial Version
+# 08/21/2015 - Dan West - Added optional arguments for specifying the beverage type and number to start with
 #
 # Notes
 # I just needed something to test Github
-#
-# Variables
-# - variablename - Description
 #
 # Known Issues/ToDo
 # - Finish Writing the Script
@@ -22,11 +21,11 @@ import sys, time, argparse
 # Check for arguments
 def arg_check():
     parser = argparse.ArgumentParser(prog='surlybot.py')
-    parser.add_argument('-b', '--beverage', required=False, action='store', dest='bev_type', help='Requires a valid beverage')
-    parser.add_argument('-n', '--number', required=False, action='store', dest='bottle_num', help='Requires a number greater than 0')
+    parser.add_argument('-b', '--beverage', default='beer', required=False, action='store', dest='bev_type', help='Requires a valid beverage')
+    parser.add_argument('-n', '--number', default='10', required=False, action='store', dest='bottle_num', help='Requires a number greater than 0')
     args = vars(parser.parse_args())
-    bev_type = args['bev_type']
-    bottle_num = args['bottle_num']
+    bev_type = str(args['bev_type'])
+    bottle_num = int(args['bottle_num'])
     return bev_type, bottle_num
 
 # Sing the song
@@ -64,7 +63,10 @@ def singing(bev_type, bottle_num):
 
 # Code goes below here
 def main():
-	bev_type, bottle_num = int(arg_check())
+	# Check optional arguments
+	bev_type, bottle_num = arg_check()
+
+	# Start singing the song
 	singing(bev_type, bottle_num)
 
 # Run the main script
